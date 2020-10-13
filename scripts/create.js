9e8520763aad773a983d3a2fd2e8c8261d6fbbfd
Vineeth S <svineth.face@gmail.com>
2020-10-13 18:20:03 +0530

    freeze boilerplate version

--- scripts/create.js
+++ scripts/create.js
@@ -4,7 +4,7 @@ const {projectName, displayName} = require('import-cwd')('./config.json');
 const fs = require('fs');
 
 function create(cb) {
-  var process = spawn("npx",["react-native","init",projectName,"--title",`\"${displayName}\"`,"--skip-install","--template","react-native-template-typescript"]);
+  var process = spawn("npx",["react-native","init",projectName,"--title",`\"${displayName}\"`,"--skip-install","--template","react-native-template-typescript@6.5.*"]);
   spinners.add('create',{text:"Creating front-end boilerplate"});
   process.on('exit', () => {
     fs.promises.access(`${projectName}/package.json`)
